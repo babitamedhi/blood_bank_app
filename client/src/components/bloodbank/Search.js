56be30b91c960b227edbb3a706858aad1e4ae40e
Ritisha Deka <128310447+Beerits@users.noreply.github.com>
2024-04-14 20:24:46 +0530

    Live Location Added

--- client/src/components/bloodbank/Search.js
+++ client/src/components/bloodbank/Search.js
@@ -1,3 +1,81 @@
+// import React, { useState, useEffect } from "react";
+// import Axios from "axios";
+
+// //css
+// import "../../assets/css/Search.css";
+
+// const Search = () => {
+//   //variables
+//   var [place, setplace] = useState("");
+//   var [blood, setblood] = useState("");
+//   const [searchList, setsearchList] = useState([]);
+
+//   //search for blood
+//   useEffect(() => {
+//     Axios.post("http://localhost:3001/home/search", {
+//       place: place,
+//       blood: blood,
+//     }).then((response) => {
+//       if (response.data.message) {
+//         //alert(response.data.message);
+//       } else {
+//         setsearchList(response.data);
+//       }
+//     });
+//   },[blood, place]);
+
+//   //returning
+//   return (
+//     <div className="search">
+//       {" "}
+//       <form>
+//         <input
+//           type="text"
+//           placeholder="PLACE"
+//           name="place"
+//           onChange={(e) => {
+//             setplace(e.target.value);
+//           }}
+//         />
+//         <input
+//           type="text"
+//           placeholder="BLOOD GROUP"
+//           name="bloodGroup"
+//           onChange={(e) => {
+//             setblood(e.target.value);
+//           }}
+//         />
+//         {/* <button onClick={()=>useEffect}><i className="fa fa-search"/></button> */}
+//       </form>
+//       <table className="blood-table">
+//         <thead>
+//           <tr>
+//             <th>Name</th>
+//             <th>Phone </th>
+//             <th>Place</th>
+//             <th>BloodGroup</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {searchList.map((val,i) => {
+//             return (
+//               <tr key={i}>
+//                 <td>{val.userFName}</td>
+//                 <td>{val.userPhone}</td>
+//                 <td>{val.userPlace}</td>
+//                 <td>{val.userBloodGroup}</td>
+//               </tr>
+//             );
+//           })}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
+// export default Search;
+
+
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 
@@ -6,46 +84,83 @@ import "../../assets/css/Search.css";
 
 const Search = () => {
   //variables
-  var [place, setplace] = useState("");
-  var [blood, setblood] = useState("");
-  const [searchList, setsearchList] = useState([]);
+  const [place, setPlace] = useState("");
+  const [blood, setBlood] = useState("");
+  const [searchList, setSearchList] = useState([]);
 
-  //search for blood
+  // Function to handle getting the user's location
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lat = position.coords.latitude;
+          const lon = position.coords.longitude;
+          // Perform reverse geocoding to get the city or district name
+          Axios.get(
+            `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&apiKey=4e49ccc3eb784d4d84cf5b6e6ca2eca2`
+          )
+            .then((response) => {
+              // Get the city or district name from the response
+              const cityName = response.data.features[0].properties.city;
+              // Update the place state variable with the obtained city or district name
+              setPlace(cityName);
+            })
+            .catch((error) => {
+              console.error("Error getting location:", error);
+            });
+        },
+        (error) => {
+          console.error("Error getting location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  };
+
+  // Search for blood
   useEffect(() => {
     Axios.post("http://localhost:3001/home/search", {
       place: place,
       blood: blood,
-    }).then((response) => {
-      if (response.data.message) {
-        //alert(response.data.message);
-      } else {
-        setsearchList(response.data);
-      }
-    });
-  },[blood, place]);
+    })
+      .then((response) => {
+        if (response.data.message) {
+          //alert(response.data.message);
+        } else {
+          setSearchList(response.data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error searching for blood:", error);
+      });
+  }, [blood, place]);
 
-  //returning
+  // Return
   return (
     <div className="search">
-      {" "}
       <form>
         <input
           type="text"
           placeholder="PLACE"
           name="place"
+          value={place}
           onChange={(e) => {
-            setplace(e.target.value);
+            setPlace(e.target.value);
           }}
         />
         <input
           type="text"
           placeholder="BLOOD GROUP"
           name="bloodGroup"
+          value={blood}
           onChange={(e) => {
-            setblood(e.target.value);
+            setBlood(e.target.value);
           }}
         />
-        {/* <button onClick={()=>useEffect}><i className="fa fa-search"/></button> */}
+        <button type="button" onClick={getLocation}>
+          Get Location
+        </button>
       </form>
       <table className="blood-table">
         <thead>
@@ -57,7 +172,7 @@ const Search = () => {
           </tr>
         </thead>
         <tbody>
-          {searchList.map((val,i) => {
+          {searchList.map((val, i) => {
             return (
               <tr key={i}>
                 <td>{val.userFName}</td>
@@ -74,3 +189,4 @@ const Search = () => {
 };
 
 export default Search;
+
